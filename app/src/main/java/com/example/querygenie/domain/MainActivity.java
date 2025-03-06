fb6bd758855abad52e073906a956dff8e50b7237
OgurecRoman <treninasweets@gmail.com>
2025-03-06 15:43:58 +0300

    set notifications!!! every hour

--- app/src/main/java/com/example/querygenie/domain/MainActivity.java
+++ app/src/main/java/com/example/querygenie/domain/MainActivity.java
@@ -1,15 +1,17 @@
 package com.example.querygenie.domain;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.os.Bundle;
-
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import com.example.querygenie.R;
+import com.example.querygenie.domain.receivers.NotificationReceiver;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
@@ -22,15 +24,28 @@ public class MainActivity extends AppCompatActivity {
 
         BottomNavigationView nav_view = findViewById(R.id.nav_view);
         NavController navController = Navigation.findNavController(this, R.id.my_nav_graph);
-//        AppBarConfiguration appBarConfiguration =
-//                new AppBarConfiguration.Builder(navController.getGraph())
-//                        .build();
-        //NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(nav_view, navController);
+
+        showNotifications();
     }
 
     @Override
     public boolean onSupportNavigateUp() {
         return Navigation.findNavController(this, R.id.my_nav_graph).navigateUp();
+    }
+
+    public void showNotifications() {
+        int flag = PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_IMMUTABLE;
+        Intent intent = new Intent(this, NotificationReceiver.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(this, 0, intent, flag);
+        AlarmManager alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
+
+        alarmManager.setRepeating(
+                AlarmManager.RTC,
+                System.currentTimeMillis() + 1000 * 60 * 60,
+                1000 * 60 * 60,
+                pendingIntent
+        );
     }
 }
